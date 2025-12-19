6b719eaad677982327bcee80fd4f771bb4a433a0
MINING123STUDIOs <124891784+MINING123STUDIOs@users.noreply.github.com>
2025-12-19 13:20:11 +0100

    Update sim_API.py

--- sim_API.py
+++ sim_API.py
@@ -1,3 +1,16 @@
+"""
+Contents:
+    imports
+    misl. crypto utility functions
+    string & print functions
+    numerical analysis functions
+    misl. helper functions
+    main simulation functions
+    UI functions
+    Notes
+"""
+
+#imports:
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sci
@@ -9,6 +22,8 @@ import time
 import hashlib
 from cryptography.fernet import Fernet
 
+#misl. crypto utility functions:
+
 def c_encrypt(key, massage):
     f = Fernet(key)
     return f.encrypt(massage.encode("utf-8"))
@@ -17,22 +32,20 @@ def c_decrypt(key, digest):
     f = Fernet(key)
     return f.decrypt(digest).decode("utf-8")
 
-def readfile(name):
-    with open(name) as f: tmp = f.read()
-    return tmp
-
-def imp():
-    print("This feature will be implemented soon.")
-
 def MD5(strg):
     return hashlib.md5(strg.encode("utf8")).hexdigest()
 
 def SHA256(strg):
     return hashlib.sha256(strg.encode("utf8")).hexdigest()
 
+#string & print functions:
+
+def imp():
+    print("This feature will be implemented soon.")
+
 def clean(text):
     allowed = string.ascii_letters + string.digits + "_-."
-    return "" if not all ( ch in allowed for ch in text ) else text
+    return "" if ( not all ( ch in allowed for ch in text ) ) or text.startswith("__") else text
 
 def rngstr(length):
     chars = string.ascii_letters * 2 + string.digits + "+-*/=()!%&?#_;:.,$"
@@ -41,106 +54,18 @@ def rngstr(length):
 def norm_solver_name(name): 
     return name.replace("eE", "explicit_Euler").replace("iE", "implicit_Euler").replace("GLRK", "Gauss_Legendre_Runge_Kutta_").replace("RK", "Runge_Kutta_").replace("_", " ")
 
-def console(scope):
-    while True:
-        cinp = input(">>> ").casefold().strip()
-        lb()
-        if cinp == "exit":
-            print("Exited console.")
-            lb()
-            break
-        elif cinp == "set":
-            var = input("Enter the name of the variable you want to set: ")
-            val = input("Enter the value you want to set the variable to: ")
-            lb()
-            val = clean(val)
-            var = clean(var)
-            scope[var] = ( 1 * ( val ) )
-            print(f"Set {var} to {val}.")
-            lb()
-        elif cinp == "inspect" or cinp == "ins":
-            var = input("Enter the name of the variable you want to inspect: ")
-            lb()
-            if var in scope:
-                print(scope[var])
-            else:
-                print(f"No variable with the name \"{var}\" is defined.")
-            lb()
-        elif cinp == "stringset" or cinp == "strset":
-            var = input("Enter the name of the variable you want to set: ")
-            val = input("Enter the string you want to set the variable to: ")
-            lb()
-            val = clean(val)
-            var = clean(var)
-            scope[var] = val
-            print(f"Set {var} to {val}.")
-            lb()
-        elif cinp == "help":
-            lb()
-            print("Help menu:")
-            print("Available commands: set, string set, ins, exit")
-        else:
-            print("Invalid command!")
-            lb()
+def lb():
+    print("")
 
-def UI(DeltaTime, Burntime, Timeframe, Enable_console, Confirm_num_len, scope):
-    lb()
-    print("Done.")
-    lb()
-    INP = "Y"
-    
-    imp_s = ["implicit Euler", "Gauss Legendre Runge Kutta 2", "Gauss Legendre Runge Kutta 4", "Gauss Legendre Runge Kutta 6"]
-    
-    burnsteps =  int( Burntime / DeltaTime)
-    steps = int(( Timeframe )/DeltaTime)
-    
-    y = True
-    while y == True:
-        INP = input(f"Confirm simulating {steps + burnsteps} samples? [Y]/[N]/[i]").casefold().strip()
-        if INP in [ "y", "n" ]:
-            y = False
-            break
-        if INP == "i":
-            ODEsolver, EQsolver = scope["ODEsolver"], scope["EQsolver"]
-            Timeframe, DeltaTime, Burntime = scope["Timeframe"], scope["DeltaTime"], scope["Burntime"]
-            Save_Data, Save_Format, Save_Filename = scope["Save_Data"], scope["Save_Format"], scope["Save_Filename"]
-            SuppHash, FileHash = scope["SuppHash"], scope["FileHash"]
-            steps, burnsteps = int( Timeframe / DeltaTime ), int( Burntime  / DeltaTime )
-            info = f"""
+def progress(percent):
+    percent = np.clip(percent, a_min=0, a_max=100)
+    bar_len = 50
+    filled = int(bar_len * percent / 100)
+    bar = "█" * filled + " " * (bar_len - filled)
+    sys.stdout.write(f"\r|{bar}| {percent:.0f}%")
+    sys.stdout.flush()
 
-The Timestep in the simulation is set to {DeltaTime} seconds.
-Simulating {steps + burnsteps} samples. {burnsteps} samples will be discarded ({Burntime} seconds), {steps} samples will be recorded ({Timeframe} seconds).
-Using {ODEsolver}{ f" with {EQsolver}" if ODEsolver in imp_s else "" }.
-The resulting data will {"" if Save_Data == True else "not"}be saved to disk {f" in a {Save_Format} file named {Save_Filename}{Save_Format}" if Save_Data == True else ""}.
-The SHA256 of the current file is                {FileHash} .
-The SHA256 of the current file is supposed to be {SuppHash} .
-{"The current hash does NOT match the supposed hash. This indicates that the file has been modified since the last update of the supposed hash." if FileHash != SuppHash else ""}
-"""
-            print(info)
-        elif ( INP == "console" or INP == "con" ) and Enable_console == True:
-            RNGVAR = rngstr(Confirm_num_len)
-            lb()
-            print("Warning: usage of this function may break the softwear!")
-            cinp = input(f"To confirm entering the console please enter the following key: \n{RNGVAR} \n")
-            if cinp == RNGVAR:
-                lb()
-                print("Console:") # The console is so restrictive, that its safe.
-                lb()
-                console(scope)
-            else:
-                lb()
-                print("Incorrect numer!")
-                lb()
-                pass
-        elif INP == "" :
-            pass
-        else:
-            print("Wrong input, please try again.")
-
-    if INP == "y":
-        pass
-    else: 
-        exit()
+#numerical analysis functions:
 
 def newton_solve(F, x0, tol=1e-9, max_iter=20):
     x = x0.astype(float).copy()
@@ -169,62 +94,6 @@ def scalar_solve(F, x0, tol=1e-9, max_iter=20):
         if abs(F(x)) < tol:
             return x
     return x
-
-def linint(hi, lo, s):
-    if s < 0.0:
-        s = 0.0
-    elif s > 1.0:
-        s = 1.0
-    return hi * s + lo * ( 1 - s )
-
-def thermrad(A, emiss, T, T_amb):
-    sigma = 5.670374419e-8
-    return A * emiss * sigma * ( ( T + 273.15 ) ** 4 - ( T_amb + 273.15 ) ** 4 )
-
-def clock(t, type, hi=1, lo=-1, f=1, duty=0.5, phase=0):
-    if type == "square" or type == "sq":
-        return hi if ( ( f * t + phase ) % 1 ) > duty else lo
-    if type == "sine" or type == "sn":
-        return linint( hi, lo, 0.5 + 0.5 * np.sin( 2 * np.pi * ( f * t + phase ) ) )
-    if type == "triangle" or type == "tg":
-        return linint( hi, lo, 2 * np.abs( ( f * t + phase ) % 1 - 0.5 ) )
-    if type == "saw" or type == "sw":
-        return linint( hi, lo, ( f * t + phase ) % 1 )
-    if type == "white_noise" or type == "wn":
-        return linint( hi, lo, rng.random() )
-    if type == "0":
-        return linint( hi, lo, 0 )
-    else:
-        return 0
-
-def progress(percent):
-    percent = np.clip(percent, a_min=0, a_max=100)
-    bar_len = 50
-    filled = int(bar_len * percent / 100)
-    bar = "█" * filled + " " * (bar_len - filled)
-    sys.stdout.write(f"\r|{bar}| {percent:.0f}%")
-    sys.stdout.flush()
-
-def safeexp(x):
-     return np.exp(np.clip(x, a_max=700, a_min=-750))
-
-def lb():
-    print("")
-
-def set_const(SuppHash, sim_name="sim.py", config_name="config.ini"):
-    config = readfile(f"{config_name}")
-    FC = f"\n\n#{sim_name}: \n\n" + readfile(f"{sim_name}").replace(SuppHash, "") + "\n\n#sim_API.py: \n\n" + readfile("sim_API.py") + f"\n\n#{config_name}: \n\n" + config
-    FileHash = SHA256(FC)
-    StartTime = time.perf_counter()
-    return config, FC, FileHash, StartTime
- 
-def save_file(Save_Data, Save_Format, Save_Filename, D):
-    if Save_Format == ".npz" and Save_Data == True:
-        np.savez(f"{Save_Filename}.npz", D)
-    if Save_Format == ".txt" and Save_Data == True:
-        np.savetxt(f"{Save_Filename}.txt", D)
-    if Save_Format == ".csv" and Save_Data == True:
-        np.savetxt(f"{Save_Filename}.csv", D, delimiter=",")
 
 def eqsolve(F, G0, EQsolver):
         if EQsolver == "custom_Newton" or EQsolver == "cN":
@@ -307,31 +176,75 @@ def step(df, t, dState, State, ODEsolver, DeltaTime, EQsolver):
         
         return dState + DeltaTime * ( 5 / 18 * k1 + 4 / 9 * k2 + 5 / 18 * k3 )
 
-def plot(plot_type, d, datalab, xlab="Time in s", ylab="Y-axis", dlab="Diagram", logy=False ):
-    if plot_type == "Graph":
-        w = np.array(np.shape(d))
-        w = int(w[0] - 1)
-        print(w)
-        fig, ax = plt.subplots(layout='constrained')
-        for plot_num in range(w):
-            x, y = d[ w - 1, : ], d[ w, : ]
-            ax.plot(x, y, label=datalab[int( plot_num / 2 - 1 )])
-        
-        ax.set(xlabel=xlab, ylabel=ylab,
-            title=dlab)
-        if logy:
-            ax.set_yscale("log")
-        plt.tick_params(axis="both", which="both")
-        ax.grid()
+def integrate(f, a, b, s): # \int_{a}^{b}f(x)dx
+    dx = ( b - a ) / s
+    x = a
+    c = 0
+    fc = f( a )
+    fn = f( a + dx )
+    for _ in range(s):
+        c += dx * 0.5 * ( fc + fn )
+        x += dx
+        fc = fn
+        if _ < s - 1:
+            fn = f( x + dx )
+    return c
 
-        fig.savefig("test.png")
-        print("Done.")
-        plt.show()
+#misl. helper functions:
 
-    elif plot_type == "Animation":
-        imp()
-    
-    input("The end of the programm was reached. Press enter to exit.")
+def safeexp(x):
+     return np.exp(np.clip(x, a_max=700, a_min=-750))
+
+def linint(hi, lo, s):
+    if s < 0.0:
+        s = 0.0
+    elif s > 1.0:
+        s = 1.0
+    return hi * s + lo * ( 1 - s )
+
+def readfile(name):
+    with open(name) as f: tmp = f.read()
+    return tmp
+
+def thermrad(A, emiss, T, T_amb):
+    sigma = 5.670374419e-8
+    return A * emiss * sigma * ( ( T + 273.15 ) ** 4 - ( T_amb + 273.15 ) ** 4 )
+
+def clock(t, type, hi=1, lo=-1, f=1, duty=0.5, phase=0):
+    if type == "square" or type == "sq":
+        return hi if ( ( f * t + phase ) % 1 ) > duty else lo
+    if type == "sine" or type == "sn":
+        return linint( hi, lo, 0.5 + 0.5 * np.sin( 2 * np.pi * ( f * t + phase ) ) )
+    if type == "triangle" or type == "tg":
+        return linint( hi, lo, 2 * np.abs( ( f * t + phase ) % 1 - 0.5 ) )
+    if type == "saw" or type == "sw":
+        return linint( hi, lo, ( f * t + phase ) % 1 )
+    if type == "white_noise" or type == "wn":
+        return linint( hi, lo, rng.random() )
+    if type == "0":
+        return linint( hi, lo, 0 )
+    else:
+        return 0
+
+def save_file(Save_Data, Save_Format, Save_Filename, D):
+    if Save_Format == ".npz" and Save_Data == True:
+        np.savez(f"{Save_Filename}.npz", D)
+    if Save_Format == ".txt" and Save_Data == True:
+        np.savetxt(f"{Save_Filename}.txt", D)
+    if Save_Format == ".csv" and Save_Data == True:
+        np.savetxt(f"{Save_Filename}.csv", D, delimiter=",")
+
+def no_f(t, x, s):
+    return s
+
+def set_const(SuppHash, sim_name="sim.py", config_name="config.ini"):
+    config = readfile(f"{config_name}")
+    FC = f"\n\n#{sim_name}: \n\n" + readfile(f"{sim_name}").replace(SuppHash, "") + "\n\n#sim_API.py: \n\n" + readfile("sim_API.py") + f"\n\n#{config_name}: \n\n" + config
+    FileHash = SHA256(FC)
+    StartTime = time.perf_counter()
+    return config, FC, FileHash, StartTime
+
+#main simulation functions:
 
 def run_sim(DeltaTime, State, dState, Timeframe, Burntime, f, df, ODEsolver, EQsolver, Show_bar = True, Enable_UI = True):
     
@@ -371,28 +284,139 @@ def run_sim(DeltaTime, State, dState, Timeframe, Burntime, f, df, ODEsolver, EQs
         print("Please wait. . .")
     return Rec
 
-def integrate(f, a, b, s): # \int_{a}^{b}f(x)dx
-    dx = ( b - a ) / s
-    x = a
-    c = 0
-    fc = f( a )
-    fn = f( a + dx )
-    for _ in range(s):
-        c += dx * 0.5 * ( fc + fn )
-        x += dx
-        fc = fn
-        if _ < s - 1:
-            fn = f( x + dx )
-    return c
+def plot(plot_type, d, datalab, xlab="Time in s", ylab="Y-axis", dlab="Diagram", logy=False ):
+    if plot_type == "Graph":
+        w = np.array(np.shape(d))
+        w = int(w[0] - 1)
+        print(w)
+        fig, ax = plt.subplots(layout='constrained')
+        for plot_num in range(w):
+            x, y = d[ w - 1, : ], d[ w, : ]
+            ax.plot(x, y, label=datalab[int( plot_num / 2 - 1 )])
+        
+        ax.set(xlabel=xlab, ylabel=ylab,
+            title=dlab)
+        if logy:
+            ax.set_yscale("log")
+        plt.tick_params(axis="both", which="both")
+        ax.grid()
 
-def no_f(t, x, s):
-    return s
+        fig.savefig("test.png")
+        print("Done.")
+        plt.show()
+
+    elif plot_type == "Animation":
+        imp()
     
+    input("The end of the programm was reached. Press enter to exit.")
+
+#UI functions:
+
+def console(scope):
+    while True:
+        cinp = input(">>> ").casefold().strip()
+        lb()
+        if cinp == "exit":
+            print("Exited console.")
+            lb()
+            break
+        elif cinp == "set":
+            var = input("Enter the name of the variable you want to set: ")
+            val = input("Enter the value you want to set the variable to: ")
+            lb()
+            val = clean(val)
+            var = clean(var)
+            scope[var] = val
+            print(f"Set {var} to {val}.")
+            lb()
+        elif cinp == "inspect" or cinp == "ins":
+            var = input("Enter the name of the variable you want to inspect: ")
+            lb()
+            if var in scope:
+                print(scope[var])
+            else:
+                print(f"No variable with the name \"{var}\" is defined.")
+            lb()
+        elif cinp == "stringset" or cinp == "strset":
+            var = input("Enter the name of the variable you want to set: ")
+            val = input("Enter the string you want to set the variable to: ")
+            lb()
+            val = clean(val)
+            var = clean(var)
+            scope[var] = f"{val}"
+            print(f"Set {var} to {val}.")
+            lb()
+        elif cinp == "help":
+            lb()
+            print("Help menu:")
+            print("Available commands: set, string set, ins, exit")
+        else:
+            print("Invalid command!")
+            lb()
+
+def UI(Enable_console, Confirm_num_len, scope):
+    lb()
+    print("Done.")
+    lb()
+    INP = "Y"
+    
+    Timeframe, DeltaTime, Burntime = scope["Timeframe"], scope["DeltaTime"], scope["Burntime"]
+    steps, burnsteps = int( Timeframe / DeltaTime ), int( Burntime  / DeltaTime )
+    
+    imp_s = ["implicit Euler", "Gauss Legendre Runge Kutta 2", "Gauss Legendre Runge Kutta 4", "Gauss Legendre Runge Kutta 6"]
+    
+    while True:
+        INP = input(f"Confirm simulating {int( (Timeframe + Burntime) / DeltaTime )} samples? [Y]/[N]/[i]").casefold().strip()
+        if INP in [ "y", "n" ]:
+            break
+        if INP == "i":
+            ODEsolver, EQsolver = norm_solver_name(scope["ODEsolver"]), scope["EQsolver"]
+            Timeframe, DeltaTime, Burntime = scope["Timeframe"], scope["DeltaTime"], scope["Burntime"]
+            Save_Data, Save_Format, Save_Filename = scope["Save_Data"], scope["Save_Format"], scope["Save_Filename"]
+            SuppHash, FileHash = scope["SuppHash"], scope["FileHash"]
+            steps, burnsteps = int( Timeframe / DeltaTime ), int( Burntime  / DeltaTime )
+            info = f"""
+
+The Timestep in the simulation is set to {DeltaTime} seconds.
+Simulating {steps + burnsteps} samples. {burnsteps} samples will be discarded ({Burntime} seconds), {steps} samples will be recorded ({Timeframe} seconds).
+Using {ODEsolver}{ f" with {EQsolver}" if ODEsolver in imp_s else "" }.
+The resulting data will {"" if Save_Data == True else "not"}be saved to disk {f" in a {Save_Format} file named {Save_Filename}{Save_Format}" if Save_Data == True else ""}.
+The SHA256 of the current file is                {FileHash} .
+The SHA256 of the current file is supposed to be {SuppHash} .
+{"The current hash does NOT match the supposed hash. This indicates that the file has been modified since the last update of the supposed hash." if FileHash != SuppHash else ""}
 """
-Recommended Project configuration:
-Project (Folder)
-│
-├──sim.py
-├──sim_API.py
-└──config.ini
+            print(info)
+        elif ( INP == "console" or INP == "con" ) and Enable_console == True:
+            RNGVAR = rngstr(Confirm_num_len)
+            lb()
+            print("Warning: usage of this function may break the softwear!")
+            cinp = input(f"To confirm entering the console please enter the following key: \n{RNGVAR} \n")
+            if cinp == RNGVAR:
+                lb()
+                print("Console:") # The console is so restrictive, that its safe
+                lb()
+                console(scope)
+            else:
+                lb()
+                print("Incorrect numer!")
+                lb()
+                pass
+        elif INP == "" :
+            pass
+        else:
+            print("Wrong input, please try again.")
+
+    if INP == "y":
+        pass
+    else: 
+        exit()
+
+#Notes:
 """
+    Recommended Project configuration:
+    Project (Folder)
+    │
+    ├──sim.py
+    ├──sim_API.py
+    └──config.ini
+"""
