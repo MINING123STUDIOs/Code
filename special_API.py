c5b631e3f2c2e7b0f7a88821e08e8db43f46e17b
MINING123STUDIOs <124891784+MINING123STUDIOs@users.noreply.github.com>
2025-12-22 17:30:24 +0100

    Update special_API.py

--- special_API.py
+++ special_API.py
@@ -1,7 +1,10 @@
 """
 Contents:
     imports
-    functions
+    crypto functions
+    physical modeling functions
+    helper functions
+    misc. signal functions
     Notes
 """
 
@@ -13,6 +16,8 @@ import hashlib
 import string
 from cryptography.fernet import Fernet
 from math_API import *
+
+#crypto functions:
 
 def c_encrypt(key, massage):
     f = Fernet(key)
@@ -28,6 +33,15 @@ def MD5(strg):
 def SHA256(strg):
     return hashlib.sha256(strg.encode("utf8")).hexdigest()
 
+#helper functions:
+
+def readfile(name):
+    with open(name) as f: tmp = f.read()
+    return tmp
+
+def norm_solver_name(name): 
+    return name.replace("eE", "explicit_Euler").replace("iE", "implicit_Euler").replace("GLRK", "Gauss_Legendre_Runge_Kutta_").replace("RK", "Runge_Kutta_").replace("_", " ")
+
 def clean(text):
     allowed = string.ascii_letters + string.digits + "_-."
     return "" if ( not all ( ch in allowed for ch in text ) ) or text.startswith("__") else text
@@ -36,19 +50,42 @@ def rngstr(length):
     chars = string.ascii_letters * 2 + string.digits + "+-*/=()!%&?#_;:.,$"
     return "".join( rng.choice( chars ) for _ in range( length ) )
 
-def norm_solver_name(name): 
-    return name.replace("eE", "explicit_Euler").replace("iE", "implicit_Euler").replace("GLRK", "Gauss_Legendre_Runge_Kutta_").replace("RK", "Runge_Kutta_").replace("_", " ")
-
 def safeexp(x):
      return np.exp(np.clip(x, a_max=700, a_min=-750))
 
-def readfile(name):
-    with open(name) as f: tmp = f.read()
-    return tmp
+
+
+#physical modeling functions:
 
 def thermrad(a, emiss, t, t_amb):
     sigma = 5.670374419e-8
     return a * emiss * sigma * ( ( t + 273.15 ) ** 4 - ( t_amb + 273.15 ) ** 4 )
+    
+def double_pendulum_ode(t, x, s, m1, m2, l1, l2, g):
+    theta1, theta2, w1, w2 = x[0], x[1], x[2], x[3]
+    
+    delta = theta1 - theta2
+    
+    dtheta1 = w1
+    dtheta2 = w2
+    
+    Div = ( 2 * m1 + m2 - m2 * np.cos( 2 * delta ) )
+    
+    dw1 = ( - g * ( 2 * m2 + m1 ) * np.sin( theta1 ) - m2 * g * np.sin( theta1 - 2 * theta2 ) - 2 * np.sin( delta ) * m2 * ( w2 ** 2 * l2 + w1 ** 2 * l1 * np.cos( delta ) ) ) / ( l1 * Div )
+    dw2 = ( 2 * np.sin( delta ) * ( w1 ** 2 * l1 * ( m1 + m2 ) + g * ( m1 + m2 ) * np.cos( theta1 ) + w2 ** 2 * l2 * m2 * np.cos( delta ) ) ) / ( l2 * Div )
+    
+    dx = np.zeros_like(x)
+    dx[0], dx[1], dx[2], dx[3] = dtheta1, dtheta2, dw1, dw2
+    return dx
+
+def dielectric_breakdown(u, i_brk, u_brk, u_beta ):
+    return ( i_brk / u_beta ** u_brk ) * ( u_beta ** ( u ) - u_beta ** ( - u ) )
+
+def diode(u, i_sat, u_t):
+    i = i_sat * ( safeexp( u / u_t ) - 1)
+    return i
+
+#misc. signal functions:
 
 def clock(t, type, hi=1, lo=-1, f=1, duty=0.5, phase=0):
     if type == "square" or type == "sq":
@@ -65,26 +102,6 @@ def clock(t, type, hi=1, lo=-1, f=1, duty=0.5, phase=0):
         return linint( hi, lo, 0 )
     else:
         return 0
-
-def double_pendulum_ode(t, x, s, m1, m2, l1, l2, g):
-    theta1, theta2, w1, w2 = x[0], x[1], x[2], x[3]
-    
-    delta = theta1 - theta2
-    
-    dtheta1 = w1
-    dtheta2 = w2
-    
-    Div = ( 2 * m1 + m2 - m2 * np.cos( 2 * delta ) )
-    
-    dw1 = ( - g * ( 2 * m2 + m1 ) * np.sin( theta1 ) - m2 * g * np.sin( theta1 - 2 * theta2 ) - 2 * np.sin( delta ) * m2 * ( w2 ** 2 * l2 + w1 ** 2 * l1 * np.cos( delta ) ) ) / ( l1 * Div )
-    dw2 = ( 2 * np.sin( delta ) * ( w1 ** 2 * l1 * ( m1 + m2 ) + g * ( m1 + m2 ) * np.cos( theta1 ) + w2 ** 2 * l2 * m2 * np.cos( delta ) ) ) / ( l2 * Div )
-    
-    dx = np.zeros_like(x)
-    dx[0], dx[1], dx[2], dx[3] = dtheta1, dtheta2, dw1, dw2
-    return dx 
-
-def dielectric_breakdown(u, i_brk, u_brk, u_beta ):
-    return ( i_brk / u_beta ** u_brk ) * ( u_beta ** ( u ) - u_beta ** ( - u ) )
 
 def square(x):
     return np.sign( np.sin( np.pi * x ) )
