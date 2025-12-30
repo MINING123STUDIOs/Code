d0a949e94d64dd03588aeeb298ee21f8bd169b86
MINING123STUDIOs <124891784+MINING123STUDIOs@users.noreply.github.com>
2025-12-30 23:16:13 +0100

    Delete z.py

--- z.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from sim_API import *
-from special_API import *
-r=run_sim(2e-3,np.array([0]),np.array([0.2,0,0,0]),5,0,no_f,lambda t,x,s:double_pendulum_ode(t,x,s,1,1,1,1,9.81),"GLRK2","fS")
-plot("Graph",np.array([np.sin(r[1,:])+np.sin(r[2,:]),-np.cos(r[1,:])-np.cos(r[2,:])]),"a")
